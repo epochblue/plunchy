50a17a1f50063438695c633ebde82099d89725d3
Bill Israel <bill.israel@gmail.com>
2015-02-07 16:50:24 -0600

    Bump version
    Also cleanup PEP violations in `setup.py`.

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ long_description = open(
     )
 ).read()
 
-version ='1.0.1'
+version = '1.1.0'
 setup(
-    name = 'plunchy',
-    py_modules = ['plunchy'],
-    scripts = ['bin/plunchy'],
-    version = version,
-    description = "A simpler interface into OS X's launchctl",
-    long_description = long_description,
-    author = 'Bill Israel',
-    author_email = 'bill.israel@gmail.com',
-    url = 'https://github.com/epochblue/plunchy',
-    keywords = ['os x', 'launchctl', 'lunchy'],
-    license = 'MIT',
-    classifiers = [
+    name='plunchy',
+    py_modules=['plunchy'],
+    scripts=['bin/plunchy'],
+    version=version,
+    description="A simpler interface into OS X's launchctl",
+    long_description=long_description,
+    author='Bill Israel',
+    author_email='bill.israel@gmail.com',
+    url='https://github.com/epochblue/plunchy',
+    keywords=['os x', 'launchctl', 'lunchy'],
+    license='MIT',
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: MacOS X',
